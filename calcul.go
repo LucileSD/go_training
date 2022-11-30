054439acc617dfbbad20b17e7c79e3ab40388b25
LucileSD <4316@holbertonschool.com>
2022-11-30 15:58:00 +0100

    first try

---
+++ calcul.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int = 2, 4
+
+	fmt.Println("a + b = ", a+b)
+	fmt.Printf("%d = a + b\n", a+b)
+}
---
+++ test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var (
+        vie     int     = 20
+        nom     string  = "Default"
+        vitesse float32 = 5.4
+    )
+
+    fmt.Println("vie : ", vie)
+    fmt.Println("nom : ", nom)
+    fmt.Println("vitesse : ", vitesse)
+}
